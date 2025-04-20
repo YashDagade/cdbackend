81dac735d416f63b2d4a6ef1804130e57f816166
Yash Dagade <yash21.dagade@gmail.com>
2025-04-19 22:36:51 -0400

    on vercel

--- streams_config.py
+++ streams_config.py
@@ -45,30 +45,31 @@
 # Configuration for video streams
 
 STREAMS = [
-    # {
-    #     "id": "demo_crash1",
-    #     "url": "https://raw.githubusercontent.com/YashDagade/cdbackend_demo/main/crash1/crash1.m3u8",
-    #     "location": "Demo Crash Clip 1",
-    #     "analysis_fps": 1,
-    # },
+    {
+        "id": "demo_crash1",
+        "url": "https://cdbackend-demo-1v9lrpi39-yashdagades-projects.vercel.app/crash1/crash1.m3u8",
+        "location": "Demo Crash Clip 1",
+        "analysis_fps": 1,
+    },
     # {
     #     "id": "demo_crash2",
-    #     "url": "https://raw.githubusercontent.com/YashDagade/cdbackend_demo/main/crash2/crash2.m3u8",
+    #     "url": "https://cdbackend-demo-1v9lrpi39-yashdagades-projects.vercel.app/crash2/crash2.m3u8",
     #     "location": "Demo Crash Clip 2",
     #     "analysis_fps": 1,
     # },
     # {
     #     "id": "demo_crash3",
-    #     "url": "https://raw.githubusercontent.com/YashDagade/cdbackend_demo/main/crash3/crash3.m3u8",
+    #     "url": "https://cdbackend-demo-1v9lrpi39-yashdagades-projects.vercel.app/crash3/crash3.m3u8",
     #     "location": "Demo Crash Clip 3",
     #     "analysis_fps": 1,
     # },
     {
         "id": "demo_crash4",
-        "url": "https://raw.githubusercontent.com/YashDagade/cdbackend_demo/main/crash4/crash4.m3u8",
+        "url": "https://cdbackend-demo-1v9lrpi39-yashdagades-projects.vercel.app/crash4/crash4.m3u8",
         "location": "Demo Crash Clip 4",
         "analysis_fps": 1,
     },
+] 
     # {
     #     "id": "mn_c550",
     #     "url": "https://video.dot.state.mn.us/public/C550.stream/chunklist_w780326163.m3u8",
