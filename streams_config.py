ba011b5dfd73099b7884a91c958db2e51c3168b1
Yash Dagade <yash21.dagade@gmail.com>
2025-04-19 23:18:36 -0400

    ready

--- streams_config.py
+++ streams_config.py
@@ -47,7 +47,7 @@
 STREAMS = [
     {
         "id": "demo_crash1",
-        "url": "https://cdbackend-demo-1v9lrpi39-yashdagades-projects.vercel.app/crash1/crash1.m3u8",
+        "url": "https://cdbackend-demo.vercel.app/demo_crash1/demo_crash1.m3u8",
         "location": "Demo Crash Clip 1",
         "analysis_fps": 1,
     },
@@ -63,10 +63,10 @@ STREAMS = [
     #     "location": "Demo Crash Clip 3",
     #     "analysis_fps": 1,
     # },
-    {
-        "id": "demo_crash4",
-        "url": "https://cdbackend-demo-1v9lrpi39-yashdagades-projects.vercel.app/crash4/crash4.m3u8",
-        "location": "Demo Crash Clip 4",
-        "analysis_fps": 1,
-    },
+    # {
+    #     "id": "demo_crash4",
+    #     "url": "https://cdbackend-demo-1v9lrpi39-yashdagades-projects.vercel.app/crash4/crash4.m3u8",
+    #     "location": "Demo Crash Clip 4",
+    #     "analysis_fps": 1,
+    # },
 ]
